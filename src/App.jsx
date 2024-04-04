358a8936be7fca076954af84cf230976f8e73967
ekaterinakutovaya <e.katerina.kutovaya@yandex.ru>
2024-04-04 16:49:38 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import ButtonGradient from "./assets/svg/ButtonGradient.jsx";
+import Header from "./components/Header.jsx";
+
+const App = () => {
+
+  return (
+   <>
+     <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+       <Header/>
+     </div>
+     <ButtonGradient/>
+   </>
+  )
+}
+
+export default App
